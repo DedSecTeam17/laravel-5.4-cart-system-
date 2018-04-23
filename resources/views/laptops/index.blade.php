c0311f26cb0777ae608659c38233e0a15988fe68
mohammedElamin <melamin100@yahoo.com>
2018-04-23 21:14:04 +0300

    ch2

--- resources/views/laptops/index.blade.php
+++ resources/views/laptops/index.blade.php
@@ -36,8 +36,8 @@
                         <th>Price</th>
                         <th>Image</th>
                         <th>os</th>
-                        <th>created At</th>
-                        <th>updated At</th>
+                        <th>created at</th>
+                        <th>updated at</th>
                         <th>Edit</th>
                         <th>Delete</th>
                     </tr>
